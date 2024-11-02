bf96f35d7f0347a063f75ef82b896cf39fcc7b82
Alonso Silva <alonso.silva@gmail.com>
2024-11-02 15:15:00 +0100

    First commit

---
+++ notebook.py
@@ -0,0 +1,15 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+# ---
+
+# %%
+a=1
+a
